fbcffffe67609c06304c1673e893b07b65f5c6b1
SerpentDagger <serpentdagger.buisness@gmail.com>
2023-01-10 17:18:44 +0100

    Presentation update, remove unused imports, slide file.

--- src/project_sparse.py
+++ src/project_sparse.py
@@ -1,9 +1,7 @@
 #%% -*- coding: utf-8 -*-
 
 import copy
-from matplotlib.pyplot import *
 import numpy as np
-import timeit as ti
 
 # These three functions are used heavily in the switch method,
 # and are hopefully useful for the addition method as well
